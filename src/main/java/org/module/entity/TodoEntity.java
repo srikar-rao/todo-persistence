bb39df1b34981a3593055d664a1dc1791fbceee9
srikar-rao <67662872+srikar-rao@users.noreply.github.com>
2024-09-10 19:26:00 -0400

    master commit

---
+++ src/main/java/org/module/entity/TodoEntity.java
@@ -0,0 +1,24 @@
+package org.module.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+
+@Entity
+@Data
+@NoArgsConstructor
+public class TodoEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    private String title;
+    private String description;
+    private boolean completed;
+
+}
---
+++ src/main/java/org/module/repostiory/TodoRepository.java
@@ -0,0 +1,9 @@
+package org.module.repostiory;
+
+import org.module.entity.TodoEntity;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TodoRepository extends JpaRepository<TodoEntity, Long> {
+}
